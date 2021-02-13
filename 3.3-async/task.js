218ea38838ae9aa0a3be059781aa1fb289419c41
Alexei Zaharov <alexcs2013@mail.ru>
2021-02-13 19:29:12 +0300

    rework 3.3

--- 3.3-async/task.js
+++ 3.3-async/task.js
@@ -5,18 +5,18 @@ class AlarmClock {
     }
 
     addClock(time, callback, id) {
-        if (!!!id) {
+        if (!id) {
             throw new Error('error: no id');
         }
 
-        if (this.alarmCollection.findIndex((elem) => elem.id == id) != -1) {
+        if (this.alarmCollection.some((elem) => elem.id == id)) {
             return console.error("error: more than one id");
         }
 
         this.alarmCollection.push({
-            id: id,
-            time: time,
-            callback: callback
+            id,
+            time,
+            callback
         });
     }
 
@@ -27,25 +27,32 @@ class AlarmClock {
     }
 
     getCurrentFormattedTime() {
+        const time = new Date();
+        let timeStr;
+        if (`${time.getMinutes()}`.length < 2 || `${time.getHours()}`.length < 2) {
+            if (`${time.getHours()}`.length < 2) {
+                timeStr = 0 + `${time.getHours()}`;
+            }
+            if (`${time.getMinutes()}`.length < 2) {
+                timeStr = timeStr + ':' + 0 + `${time.getMinutes()}`;
+                return timeStr;
+            }
+            return timeStr + new Date().getMinutes();
+        }
         return `${new Date().getHours()}:${new Date().getMinutes()}`;
+       
     }
 
     start() {
-        function checkCloak(alarm) { 
-            if(`${new Date().getHours()}:${new Date().getMinutes()}` === alarm.time) {
-                return alarm.callback();
+        this.timerId = setInterval(() => this.alarmCollection.forEach(elem => {
+            if(this.getCurrentFormattedTime() === elem.time) {
+                return elem.callback();
             }
-
-            // if(this.getCurrentFormattedTime() === alarm.time) {
-            //     return alarm.callback();
-            // }
-        }
-
-        this.timerId = setInterval(() => this.alarmCollection.forEach(elem => checkCloak(elem)),1000);
+        }),1000);
     }
 
     stop() {
-        if(!!this.timerId) {
+        if(this.timerId) {
             clearInterval(this.timerId);
             this.timerId = null;
         }
@@ -57,19 +64,12 @@ class AlarmClock {
     }
 
     clearAlarms() {
-        stop();
+        this.stop();
         this.alarmCollection = [];
     }
 }
 
 const example = new AlarmClock();
-example.addClock(`${new Date().getHours()}:${new Date().getMinutes()}`, () => console.log("wake up"), 1);
-example.addClock(`${new Date().getHours()}:${new Date().getMinutes() + 1}`, () => {console.log("wake up, please"); example.removeClock(2)}, 2);
-example.addClock(`${new Date().getHours()}:${new Date().getMinutes() + 1}`, () => {console.log("wake up, please"); example.removeClock(2)}, 1);
-example.addClock(`${new Date().getHours()}:${new Date().getMinutes() + 2}`, () => {
-    console.log("hoo, insted of waking up you're sleeping");
-    example.clearAlarms();
-    example.printAlarms();
-}, 3);
+example.addClock(`20:01`, () => console.log("wake up"), 1);
 example.printAlarms();
 example.start();
